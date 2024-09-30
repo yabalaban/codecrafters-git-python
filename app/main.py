8617fbcd5835119723e26fa047ea21c9b3cf5a4d
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-09-30 20:52:40 +0100

    cat-file cmd

--- app/main.py
+++ app/main.py
@@ -1,23 +1,54 @@
 import sys
 import os
+import zlib
 
+
+class Path:
+    ROOT = '.git'
+    OBJECTS = f'{ROOT}/objects'
+    REFS = f'{ROOT}/refs'
+    HEAD = f'{ROOT}/HEAD'
+
+
+def init(_: list[any]):
+    os.mkdir(Path.ROOT)
+    os.mkdir(Path.OBJECTS)
+    os.mkdir(Path.REFS)
+    with open(Path.HEAD, "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
+
+
+def cat_file(argv: list[any]):
+    def parse_number(content, offset) -> tuple[int, int]:
+        val = 0 
+        while chr(content[offset]).isdigit():
+            val *= 10 
+            val += int(chr(content[offset]))
+            offset += 1
+        return (val, offset)
+
+    blob_sha1 = argv[3] 
+    pref, suff = blob_sha1[:2], blob_sha1[2:]
+
+    with open(f'{Path.OBJECTS}/{pref}/{suff}', 'rb') as f:
+        blob = f.read()
+    
+    content = zlib.decompress(blob)
+    if content.startswith(b'blob '):
+        offset = 5
+        (l, offset) = parse_number(content, offset)
+        # skip \0
+        offset += 1
+        data = content[offset: offset + l]
+        print(data.decode(), end='')
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
-    command = sys.argv[1]
-    if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
-    else:
-        raise RuntimeError(f"Unknown command #{command}")
+    cmd = sys.argv[1]
+    {
+        "init": init,
+        "cat-file": cat_file
+    }[cmd](sys.argv)
 
 
 if __name__ == "__main__":
